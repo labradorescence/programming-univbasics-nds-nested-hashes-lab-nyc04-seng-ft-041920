b994b1a488605f2706adfc7f6729d63cca7c4719
Greem <orangenewyorkcreativeagency@gmail.com>
2020-05-01 19:18:18 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,6 +11,7 @@ def assembled_aoh
   # Array. Ruby constants are denoted by ALL_CAPS
   result = []
   result << DON_G << JOELLE_VD << PAT_M << KATE_G << BRUCE_G
+  puts "# # # result #{result}"
   return result
 end
 
