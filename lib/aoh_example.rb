9511d7443c4a0328b891d6297f1a598466f08b31
Greem <orangenewyorkcreativeagency@gmail.com>
2020-05-01 19:22:16 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,6 +18,14 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  result = []
+  result << DON_G << JOELLE_VD << PAT_M << KATE_G << BRUCE_G
+  puts "# # # result #{result}"
+  return result
+
+
+
+
 end
 
 def aoh_lookup(aoh, row, key)
